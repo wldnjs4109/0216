f6ff2e7e1b84b42f8b1cc15ce52a126b38702020
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 16:02:27 +0900

    Create 11654.java

---
+++ 11654.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=System.in.read();
+		System.out.println(a);
+	}
+}
